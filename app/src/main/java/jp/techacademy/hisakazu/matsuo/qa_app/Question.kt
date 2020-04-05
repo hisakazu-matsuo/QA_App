a14090de3c2937fdc41b1df97117bc87ac2688c1
hisakazu-matsuo <matt@kmd.biglobe.ne.jp>
2020-04-05 10:46:18 +0900

    No.2 No.3 No.4 No.5 No.6 No.7 No.8

--- app/src/main/java/jp/techacademy/hisakazu/matsuo/qa_app/Question.kt
+++ app/src/main/java/jp/techacademy/hisakazu/matsuo/qa_app/Question.kt
@@ -3,7 +3,7 @@ package jp.techacademy.hisakazu.matsuo.qa_app
 import java.io.Serializable
 import java.util.ArrayList
 
-class Question(val title: String, val body: String, val name: String, val uid: String, val questionUid: String, val genre: Int, bytes: ByteArray, val answers: ArrayList<Answer>) : Serializable {
+class Question(val title: String, val body: String, val name: String, val uid: String, var questionUid: String, val genre: Int, bytes: ByteArray, val answers: ArrayList<Answer>) : Serializable {
     val imageBytes: ByteArray
 
     init {
